3d8ded4f78779a8d9fe835b4fe91cf47c2de4f7a
Luiznl <luizfffernando28@gmail.com>
2024-06-27 22:25:44 -0300

    subi os arquivos do meu site

---
+++ script.js
@@ -0,0 +1,6 @@
+const card = document.getElementById('card');
+
+card.addEventListener('click', () => {
+    alert('Te Amo Ana!!');
+    window.location.href = 'https://open.spotify.com/intl-pt/track/1BRJxe2ozG73rxff5jyvjv'; // link para a música "Dia Branco" de Geraldo Azevedo
+});
